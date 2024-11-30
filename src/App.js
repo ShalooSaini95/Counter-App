51c04aa7c7b703ea02d0ca2738bc7b423375677f
ShalooSaini95 <155061877+ShalooSaini95@users.noreply.github.com>
2024-11-30 11:25:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+   
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    if(count<1) return;
+    setCount(count - 1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1 className="text-center text-primary my-3 " style={{textDecoration:"underline"}}>Counter App</h1>
+    <div className=" d-flex flex-column justify-content-center align-items-center" style={{height:'60vh'}}>
+    <span className="text-primary fw-bold" style={{fontSize:"40px"}}>{count}</span><br/>
+    <br/>
+      <button onClick={increment} className="btn btn-primary w-50">Increment</button>
+      <br/>
+      <button onClick={decrement} className="btn btn-info w-50">Decrement</button>
+    </div></div>
   );
-}
+};
 
 export default App;
